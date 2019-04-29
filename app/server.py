161fc20f5dc263e0a987b3f0226323eff42f7abd
n2i95 <nabilaffo@gmail.com>
2019-04-29 21:02:10 +0000

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,8 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=10XhUc6rZsmtI6sQtwTgLmAiimI8EWkAa'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
